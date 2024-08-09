a44aaa2c28c91b73c3e5ca30be13c514a8701d24
Blagoja Nikoloski <blagojanikoloski1@gmail.com>
2024-08-09 20:26:48 +0200

    Another bug fixed

--- src/context.js
+++ src/context.js
@@ -45,7 +45,7 @@ function generateMap(letterList) {
 
 const Game = createContext(baseGame);
 export const useGame = () => useContext(Game);
-const macedonianCyrillicRegex = /^[А-БВГДЃЕЖЗЅИЈКЛЉМНЊОПРСТЌУФХЦЧЏШа-бвгдѓежзѕијијклљмнњопрстќуфхцчџш\s]+$/;
+const macedonianCyrillicRegex = /^[А-БВГДЃЕЖЗЅИЈКЛЉМНЊОПРСТЌУФХЦЧЏШа-бвгдѓежзѕијклљмнњопрстќуфхцчџш\s]+$/;
 
 function checkForErrors(change, state) {
   if (change.intent === "guess") {
