5c7cd59f359492f085fadec1d3996cd8de88b736
Blagoja Nikoloski <blagojanikoloski1@gmail.com>
2024-08-10 01:45:58 +0200

    Fixed unclickable thing

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ function App() {
     null,
     React.createElement(
       'div',
+      { className: 'title-container' },
+      React.createElement('img', { src: './logo.png', alt: 'Logo', className: 'logo' }),
+      React.createElement('h1', null, 'Зборовна Кутија')
+    ),
+    React.createElement(
+      'div',
       { className: 'row' },
       React.createElement(Box, null)
     ),
-    React.createElement('br', null),
-    React.createElement('br', null),
     React.createElement(
       'div',
       { className: 'row' },
